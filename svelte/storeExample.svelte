68cbaacd253b5e3a1bcdcd517ff264256dccb763
StrawberrySunset <strawberrysunset@protonmail.com>
2020-08-15 20:47:53 +0100

    Add custom svelte store example.

---
+++ svelte/storeExample.svelte
@@ -0,0 +1,10 @@
+import { writable } from 'svelte/store';
+
+const { subscribe, set, update } = writable(0);
+
+export const count = {
+	subscribe,
+	increment: () => update(n => n + 1),
+	decrement: () => update(n => n - 1),
+	reset: () => set(0)
+};
