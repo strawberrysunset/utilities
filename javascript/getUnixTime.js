1d8b3fab3c0295969cf399449166fe0ec47764b2
StrawberrySunset <strawberrysunset@protonmail.com>
2020-08-28 21:43:21 +0100

    Add passing of date object and modify arguments.

--- javascript/getUnixTime.js
+++ javascript/getUnixTime.js
@@ -1,12 +1,11 @@
 /**
  * Get UNIX time with specified offset.
  */
-export const getUnixTime = ({ day = 0, month = 0, year = 0}, ...args) => {
-    if (!args) return
-    let time = new Date();
-    time.setDate(time.getDate() + day);
-    time.setMonth(time.getMonth() + month);
-    time.setFullYear(time.getFullYear() + year);
-    return Math.round(time.getTime() / 1000);
+export const getUnixTime = (dateObject, offset) => {
+    dateObject.setDate(dateObject.getDate() + offset.days || 0);
+    dateObject.setMonth(dateObject.getMonth() + offset.months || 0);
+    dateObject.setFullYear(dateObject.getFullYear() + offset.years || 0);
+    const UnixTime = Math.round(dateObject.getTime() / 1000);
+    return UnixTime
 }
 
