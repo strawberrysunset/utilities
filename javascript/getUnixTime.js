7acdfa2f261c412fc45fd4b92727cacb0a72e3e9
StrawberrySunset <strawberrysunset@protonmail.com>
2020-08-28 22:28:43 +0100

    Wrap conditional or statement.

--- javascript/getUnixTime.js
+++ javascript/getUnixTime.js
@@ -1,11 +1,10 @@
 /**
  * Get UNIX time with specified offset.
  */
-export const getUnixTime = (dateObject, offset) => {
-    dateObject.setDate(dateObject.getDate() + offset.days || 0);
-    dateObject.setMonth(dateObject.getMonth() + offset.months || 0);
-    dateObject.setFullYear(dateObject.getFullYear() + offset.years || 0);
-    const UnixTime = Math.round(dateObject.getTime() / 1000);
-    return UnixTime
-}
-
+export const getUnixTime = (dateObject, offset = {}) => {
+  dateObject.setDate(dateObject.getDate() + (offset.days || 0));
+  dateObject.setMonth(dateObject.getMonth() + (offset.months || 0));
+  dateObject.setFullYear(dateObject.getFullYear() + (offset.years || 0));
+  const UnixTime = Math.floor(dateObject.getTime() / 1000);
+  return UnixTime;
+};
