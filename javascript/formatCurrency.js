f66e75e7bc3d773fd11ebcec2e270524b5843e3d
StrawberrySunset <strawberrysunset@protonmail.com>
2020-09-06 19:55:37 +0100

    Add currency symbol map.

--- javascript/formatCurrency.js
+++ javascript/formatCurrency.js
@@ -1,6 +1,9 @@
-export const formatCurrency = (symbol, value) => {
+import currencySymbolMap from './currencySymbolMap'
+
+export const formatCurrency = (currency, value) => {
+  const symbol = currencySymbolMap[currency];
   return (
-    symbol +
+    (symbol ? symbol : currency) +
     Number(value.toFixed(2)).toLocaleString(undefined, {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
